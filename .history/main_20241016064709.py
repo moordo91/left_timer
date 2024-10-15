0b77c1e242e8f143b9f1b5bf8db498e69c03cf62
Daeil Han <moordo91@gmail.com>
2024-10-16 07:04:06 +0900

    Initial commit to relaease the version 1.0.0

---
+++ .history/main_20241016064709.py
@@ -0,0 +1,5 @@
+﻿from timer_app import TimerApp
+
+if __name__ == "__main__":
+    app = TimerApp()
+    app.run()
---
+++ .history/utils_20241016064654.py
@@ -0,0 +1,6 @@
+﻿def format_time(total_seconds):
+    sign = "+" if total_seconds >= 0 else "-"
+    total_seconds = abs(total_seconds)
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{sign}{hours:02}:{minutes:02}:{seconds:02}"
